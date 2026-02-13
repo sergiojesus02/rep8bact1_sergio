fe6cbe8203b183cf321be7a8f758e546239d5698
sergiojesus02 <132232127+sergiojesus02@users.noreply.github.com>
2026-02-12 20:05:06 -0600

    Add index.php file and update README with university details

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rep8bact1_sergio
 para mostrar mis skills trabajando con git
+# Universidad Tecnologica de la Selva 
+#Ocosingo Chiapas, 2026
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sergio's page</title>
+</head>
+<body>
+    <h1>
+        Bienvenido a la pagina de Sergio
+    </h1>
+</body>
+</html>
